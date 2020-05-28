18f6eecfc76b3cc16190ab0deb6500f88ef3c5a1
K.Kiyokawa <koichi20110068@gmail.com>
2020-05-29 01:05:33 +0900

    feat: add router

---
+++ src/infrastructure/router.ts
@@ -0,0 +1,15 @@
+import * as express from "https://raw.githubusercontent.com/NMathar/deno-express/master/mod.ts";
+import { PostController } from "../interfaces/controllers/post_controller.ts";
+import { SqlConnection } from "./sql_connection.ts";
+
+const router = new express.App();
+const sqlConnection = new SqlConnection();
+const postController = new PostController(sqlConnection);
+
+router.get("/posts", async (req: express.Request, res: express.Response) => {
+  console.log("router#posts");
+  const result = await postController.getAllPosts();
+  await res.json(result);
+});
+
+export { router };

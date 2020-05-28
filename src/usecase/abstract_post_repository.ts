280f203d9a15c20afc73c513643b2dc1b217744f
K.Kiyokawa <koichi20110068@gmail.com>
2020-05-29 01:04:30 +0900

    feat: add findAll to abstract post repo

--- src/usecase/abstract_post_repository.ts
+++ src/usecase/abstract_post_repository.ts
@@ -1,5 +1,6 @@
-import { Post } from "../entities/post";
+import { Post } from "../entities/post.ts";
 
 export abstract class AbstractPostRepository {
+  abstract async findAll(): Promise<Post[]>;
   abstract async persist(post: Post): Promise<Post>;
 }

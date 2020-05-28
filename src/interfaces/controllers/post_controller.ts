68fdeadddf9587b1a916546964d4f0fce4665b6c
K.Kiyokawa <koichi20110068@gmail.com>
2020-05-29 01:05:02 +0900

    feat: add post controller

--- src/interfaces/controllers/post_controller.ts
+++ src/interfaces/controllers/post_controller.ts
@@ -1,13 +1,20 @@
-import { PostRepository } from "../database/post_repository";
-import { DBConnection } from "../database/db_connection";
-import { CreatePost } from "../../usecase/create_post";
-import { TPost } from "../../types/post";
+import { PostRepository } from "../database/post_repository.ts";
+import { AbstractDBConnection } from "../database/abstract_db_connection.ts";
+import { GetAllPosts } from "../../usecase/get_all_posts.ts";
+import { CreatePost } from "../../usecase/create_post.ts";
+import { TPost } from "../../types/post.ts";
 
 export class PostController {
   #post_repository: PostRepository;
 
-  constructor(dbConnection: DBConnection) {
+  constructor(dbConnection: AbstractDBConnection) {
     this.#post_repository = new PostRepository(dbConnection);
+  }
+
+  async getAllPosts() {
+    console.log("controllers#getAllPosts");
+    const usecase = new GetAllPosts(this.#post_repository);
+    return usecase.execute();
   }
 
   async createPost(req: { body: TPost }, res: any) {

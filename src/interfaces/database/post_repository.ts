bf497ffcd19e8533e825be28dbefca47a9654cf3
K.Kiyokawa <koichi20110068@gmail.com>
2020-05-29 01:04:50 +0900

    feat: add findAll method to post repo

--- src/interfaces/database/post_repository.ts
+++ src/interfaces/database/post_repository.ts
@@ -1,14 +1,29 @@
-import { AbstractPostRepository } from "../../usecase/abstract_post_repository";
-import { DBConnection } from "./db_connection";
-import { Post } from "../../entities/post";
+import { AbstractPostRepository } from "../../usecase/abstract_post_repository.ts";
+import { AbstractDBConnection } from "./abstract_db_connection.ts";
+import { Post } from "../../entities/post.ts";
+import { TPost } from "../../types/post.ts";
 
 export class PostRepository extends AbstractPostRepository {
-  #connection: DBConnection;
+  #connection: AbstractDBConnection;
 
-  constructor(connection: DBConnection) {
+  constructor(connection: AbstractDBConnection) {
     super();
     this.#connection = connection;
   }
+
+  async findAll() {
+    const postRows: any = await this.#connection.execute(
+      "SELECT * FROM posts",
+    )
+
+    return [...postRows].map((row) =>{
+      console.log({ row })
+      const [id, title, body] = row
+      console.log({ id, title, body })
+      return new Post({id,title,body})
+    });
+  }
+
   async persist(post: Post) {
     const savedPost = await this.#connection.execute(
       // ここにSQL書くのびみょい

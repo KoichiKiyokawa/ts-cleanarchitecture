f05272424804afc154bce711da1d8eb7fcfcc939
K.Kiyokawa <koichi20110068@gmail.com>
2020-06-01 22:26:56 +0900

    feat: add post serializer

--- src/interfaces/serializers/post_serializer.ts
+++ src/interfaces/serializers/post_serializer.ts
@@ -1,17 +1,19 @@
-import {Post}from"../../entities/post.ts"
+import { Post } from "../../entities/post.ts";
 
 export class PostSerializer {
-  private serializeSingle (post:Post) {
+  private serializeSingle(post: Post) {
     return {
       id: post.getId(),
       title: post.getTitle(),
-      body: post.getBody()
-    }
+      body: post.getBody(),
+    };
   }
+
   serialize(posts: Post | Post[]) {
-    if (Array.isArray(posts))
-      return posts.map(this.serializeSingle)
-    else
-      return this.serializeSingle(posts)
+    if (Array.isArray(posts)) {
+      return posts.map(this.serializeSingle);
+    } else {
+      return this.serializeSingle(posts);
+    }
   }
 }
